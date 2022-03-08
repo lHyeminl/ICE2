8e66325797ad8c970d69155717f30e989cae42c7
lHyeminl <johy@sheridancollege.ca>
2022-03-08 15:52:56 -0500

    Upload again

--- src/midtermreviewcodeforpartc/UnoOnline.java
+++ src/midtermreviewcodeforpartc/UnoOnline.java
@@ -27,6 +27,7 @@ public class UnoOnline
      {
        UnoOnline newPortal = new UnoOnline();
        newPortal.run();
+     
     }
      /**
      * method that takes in the User's name and chosen password
@@ -42,37 +43,40 @@ public class UnoOnline
         Scanner sc = new Scanner(System.in);
         System.out.println("please enter your desired user name:");
         String userName = sc.nextLine();
-        boolean validPassword=false;
-        String password="";
-        while(!validPassword)
-        {
-            System.out.println("Passwords must have at least 8 characters");
-            System.out.println("Passwords must have at least one special character");
-            System.out.println("Please enter your desired password:"); 
-            
-            password = sc.nextLine();
-            int specialCharCount=0;
-            //iterate over each character to see if it is a special character
-            for(int i=0;i<password.length(); i++)
-            {
-                if(!(Character.isLetterOrDigit(password.charAt(i))))
-                {
-                    //now we know there is at least one special character
-                    specialCharCount++;
-                }
-            }
-            if(specialCharCount>0 &&password.length()>7)
-            {
-                validPassword=true;
-            }
-        }//loop only ends when password is valid so now we create the User
         
-        User newUser = new User(userName, password);
-        users[userCount] = newUser;//add the new user to the user list
-        userCount++;
-        System.out.println("New User Added");
-        System.out.println("UserName: " + userName);
-        System.out.println("Password: just kidding can't show password");
+        PasswordValidator secret = new PasswordValidator();
+        secret.validation();
+//        boolean validPassword=false;
+//        String password="";
+//        while(!validPassword)
+//        {
+//            System.out.println("Passwords must have at least 8 characters");
+//            System.out.println("Passwords must have at least one special character");
+//            System.out.println("Please enter your desired password:"); 
+//            
+//            password = sc.nextLine();
+//            int specialCharCount=0;
+//            //iterate over each character to see if it is a special character
+//            for(int i=0;i<password.length(); i++)
+//            {
+//                if(!(Character.isLetterOrDigit(password.charAt(i))))
+//                {
+//                    //now we know there is at least one special character
+//                    specialCharCount++;
+//                }
+//            }
+//            if(specialCharCount>0 &&password.length()>7)
+//            {
+//                validPassword=true;
+//            }
+//        }//loop only ends when password is valid so now we create the User
+//        
+//        User newUser = new User(userName, password);
+//        users[userCount] = newUser;//add the new user to the user list
+//        userCount++;
+//        System.out.println("New User Added");
+//        System.out.println("UserName: " + userName);
+//        System.out.println("Password: just kidding can't show password");
     }//end run method
    
     
